704625eddaa623a370ed8bd67e79b18a442c719f
Idraki Muhamad <idrakimuhamad@gmail.com>
2023-01-03 22:35:12 +0800

    improvement: remove the extraneous micro app App1

--- apps/host/App.tsx
+++ apps/host/App.tsx
@@ -3,24 +3,17 @@ import { Federated } from '@callstack/repack/client';
 import React from 'react';
 import { Text, SafeAreaView } from 'react-native';
 
-const App1 = React.lazy(() => Federated.importModule('app1', './App'));
+// const App1 = React.lazy(() => Federated.importModule('app1', './App'));
 const Products = React.lazy(() => Federated.importModule('products', './App'));
-
-// const App2 = React.lazy(() => Federated.importModule('app2', './App'));
 
 export default function App() {
   return (
     <SafeAreaView>
       <Text>Host App</Text>
-      <React.Suspense fallback={<Text>Loading app1...</Text>}>
-        <App1 />
-      </React.Suspense>
+      
       <React.Suspense fallback={<Text>Loading Products...</Text>}>
         <Products />
       </React.Suspense>
-      {/* <React.Suspense fallback={<Text>Loading app2...</Text>}>
-        <App2 />
-      </React.Suspense> */}
     </SafeAreaView>
   );
 }
